378e5bb70f001bbea25c0bc5b226235f9a7dd2e4
jerry <qin.peng@desty.app>
2021-11-10 12:02:05 +0800

    upate1

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -12,7 +12,7 @@
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Tab 1 page,build first" />
     </ion-content>
   </ion-page>
 </template>
